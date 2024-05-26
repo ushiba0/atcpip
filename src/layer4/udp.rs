cba9bcd63913939e67406b65e07acc285063d82f
ushiba0 <lychnus372@gmail.com>
2024-05-26 08:49:48 +0000

    udp.rs から tokio::sync::Mutex をなくした。

--- src/layer4/udp.rs
+++ src/layer4/udp.rs
@@ -5,7 +5,6 @@ use anyhow::{ensure, Context};
 use bytes::Bytes;
 use once_cell::sync::Lazy;
 use tokio::sync::mpsc::{self, Receiver, Sender};
-use tokio::sync::Mutex;
 
 use crate::layer3::ipv4::Ipv4Frame;
 
@@ -24,10 +23,6 @@ pub struct UdpSocket {
 }
 
 impl UdpPacket {
-    // pub fn new() -> Self {
-    //     Default::default()
-    // }
-
     pub fn from_bytes(bytes: &Bytes) -> Self {
         Self {
             source_port: u16::from_be_bytes([bytes[0], bytes[1]]),
@@ -41,16 +36,16 @@ impl UdpPacket {
 
 // 下位のレイヤから UDP パケットが来たら、この Hashmap の Sender で送る。
 // Listen しているユーザーアプリケーションは Receiver で受け取る。
-pub static LISTEN_ADDRESSES: Lazy<Mutex<HashMap<u16, Sender<(Ipv4Addr, Bytes)>>>> =
+pub static PORT_MAP: Lazy<parking_lot::RwLock<HashMap<u16, Sender<(Ipv4Addr, Bytes)>>>> =
     Lazy::new(Default::default);
 
 impl UdpSocket {
-    pub async fn bind(_ip: Ipv4Addr, port: u16) -> anyhow::Result<Self> {
-        let mut portlist = LISTEN_ADDRESSES.lock().await;
-        ensure!(portlist.get(&port).is_none(), "Address already in use.");
+    pub fn bind(_ip: Ipv4Addr, port: u16) -> anyhow::Result<Self> {
+        let mut portmap = PORT_MAP.write();
+        ensure!(portmap.get(&port).is_none(), "Address already in use.");
 
         let (rx_sender, rx_receiver) = mpsc::channel::<(Ipv4Addr, Bytes)>(2);
-        portlist.insert(port, rx_sender);
+        portmap.insert(port, rx_sender);
 
         log::info!("Listening UDP on port {port}.");
         Ok(Self {
@@ -71,8 +66,15 @@ pub async fn udp_handler(mut receiver: Receiver<Ipv4Frame>) -> anyhow::Result<()
 
         // Todo: Checksum の計算
 
-        if let Some(s) = LISTEN_ADDRESSES.lock().await.get(&udp_packet.target_port) {
-            s.send((source_ip, udp_packet.payload)).await.unwrap();
-        }
+        // sender を clone して使わない場合、RwLock が sender().send().await() を跨ぐ
+        // ことになりコンパイルエラーとなる。
+        let sender = match PORT_MAP.read().get(&udp_packet.target_port) {
+            Some(v) => v.clone(),
+            None => {
+                log::warn!("[udp_handler] listen していない port 宛に UDP パケットが来た。");
+                continue;
+            }
+        };
+        sender.send((source_ip, udp_packet.payload)).await.unwrap();
     }
 }
--- src/udp_netcat.rs
+++ src/udp_netcat.rs
@@ -4,7 +4,7 @@ use anyhow::Result;
 
 pub async fn main() -> Result<()> {
     let listen_addr = Ipv4Addr::from_str("127.0.0.1")?;
-    let mut socket = crate::layer4::udp::UdpSocket::bind(listen_addr, 1234).await?;
+    let mut socket = crate::layer4::udp::UdpSocket::bind(listen_addr, 1234)?;
     loop {
         let (source_ip, bytes) = socket.recv_from().await;
         let message = std::str::from_utf8(&bytes)?;
